d084754b93a4008135f8ca2abfbd0444ca02fea4
younghoonyou <kcnoh2@hanyang.ac.kr>
2022-07-03 06:42:47 +0900

    Rotation But Move

--- 3D/figure.cpp
+++ 3D/figure.cpp
@@ -1,23 +1,28 @@
 #include "figure.h"
 #include <cmath>
-
-const int XPLUS = 30;
-const int YPLUS = 30;
+#define MIN(a,b) ((a<b)?a:b)
+// const double PI = 3.141592;
+int XPLUS = 20;
+int YPLUS = 10;
 
 void Point::Z_Rotate(int degree){
-    double rad = (double)degree * M_PI / 180;
+    double rad = ((double)degree * M_PI)/180;
     double matrix[3][3] = {
-        {cos(rad), -sin(rad), 0},
-        {sin(rad), cos(rad), 0},
-        {0, 0, 1},
-    };
+            {cos(rad), -sin(rad), 0},
+            {sin(rad), cos(rad), 0},
+            {0, 0, 1},
+        };
+    if(degree == 90){
+        matrix[0][0] = matrix[1][1] = 0;
+    }
+    // std::cout << "Degree : " << cos(rad) << ' ' << sin(rad) << '\n';
     double P[4] = {this->x, this->y, this->z, 1.0f};
     Point tmp;
     for (int i = 0; i < 3; ++i) tmp.x += (matrix[0][i] * P[i]);
     for (int i = 0; i < 3; ++i) tmp.y += (matrix[1][i] * P[i]);
     for (int i = 0; i < 3; ++i) tmp.z += (matrix[2][i] * P[i]);
     *this = tmp;
-    std::cout << (int)this->x << ' ' << (int)this->y << ' ' << (int)this->z << '\n';
+    // std::cout << (int)this->x << ' ' << (int)this->y << ' ' << (int)this->z << '\n';
     // std::cout << ceil(this->x) << ' ' << ceil(this->y) << ' ' << round(this->z) << '\n';
 }
 
@@ -52,7 +57,7 @@ void Point::X_Rotate(int degree){
     for (int i = 0; i < 4; ++i) tmp.y += matrix[1][i] * P[i];
     for (int i = 0; i < 4; ++i) tmp.z += matrix[2][i] * P[i];
     *this = tmp;
-    std::cout << (int)this->x << ' ' << (int)this->y << ' ' << (int)this->z << '\n';
+    // std::cout << (int)this->x << ' ' << (int)this->y << ' ' << (int)this->z << '\n';
 }
 
     void Point::Projection(){
@@ -68,36 +73,73 @@ void Point::X_Rotate(int degree){
         {0, 0, -1, 0},//b
         };
     double tx,ty;
-    tx = ty = 0;
+    tx = ty = 0.f;
     double P1[4] = {this->x, this->y, this->z, 1.f};
     for (int i = 0; i < 4; ++i) tx += (matrix[0][i] * P1[i]);
     for (int i = 0; i < 4; ++i) ty += (matrix[1][i] * P1[i]);
+    // std::cout << tx << ' ' << ty << '\n';
     *this = {tx, ty, 0};
     }
 
 void Line::Draw(std::vector<std::vector<char>> &m){
+    int X_change = INT32_MAX;
+    int Y_change = INT32_MAX;
     this->p1.Projection();
     this->p2.Projection();
     Point f1,f2;
     f1 = this->p1;
     f2 = this->p2;
-    // std::cout << (int)f1.x << ' ' << (int)f1.y << '\n';
-    // std::cout << (int)f2.x << ' ' << (int)f2.y << '\n';
-    int x_increase = (f2.x - f1.x);
-    int y_increase = (f2.y - f1.y);
+
+    // std::cout << '\n';
+    // std::cout << f1.x << ' ' << f1.y << ' ' << f1.z << '\n';
+
+    for (int i = 59; i >= 0; --i)
+    {
+        for (int j = 0; j < 100; ++j)
+        {
+            if(m[j][i] == '*'){
+                X_change = MIN(X_change, i);
+                Y_change = MIN(Y_change, j);
+            }
+        }
+    }
+    // XPLUS = 10 - X_change;
+    // YPLUS = 10 - Y_change;
+    // std::cout << XPLUS << ' ' << YPLUS; // std::cout << f2.x << ' ' << f2.y << ' ' << f2.z << '\n';
+    double x_increase = (f2.x - f1.x);
+    double y_increase = (f2.y - f1.y);
     if(x_increase == 0){
-        for (int i = f1.y;i<=f2.y;++i) m[f1.x + XPLUS][i + YPLUS] = '*';
+        if(f1.y < f2.y){
+            for (int i = (int)(f1.y); i <= (int)(f2.y); ++i) m[(int)(f1.x) + XPLUS][i + YPLUS] = '*';
+        }
+        else{
+            for (int i = (int)(f2.y); i <= (int)(f1.y); ++i) m[(int)(f1.x) + XPLUS][i + YPLUS] = '*';
+        }
+        // std::cout << (int)(f1.x) << ' ' << (int)(f1.y) << '\n';
         return;
     }
     else if(y_increase == 0){
-        for (int i = f1.x; i <= f2.x; ++i) m[i + XPLUS][f1.y + YPLUS] = '*';
+        if (f1.x < f2.x){
+            for (int i = (int)(f1.x); i <= (int)(f2.x); ++i)
+                m[i + XPLUS][(int)(f1.y) + YPLUS] = '*';
+        }
+        else{
+            for (int i = (int)(f2.x); i <= (int)(f1.x); ++i) m[i + XPLUS][(int)(f1.y) + YPLUS] = '*';
+        }
         return;
     }
-    double gradient = (double)(x_increase /y_increase);
+    double gradient = (double)(y_increase /x_increase);
     double remain = double(f1.y - (gradient * f1.x));
-    for (int i = f1.x; i <= f2.x; ++i){
-        int y = ceil((gradient*i) + remain);
-        std::cout << i << ' ' << y << '\n';
-        m[i + XPLUS][y + YPLUS] = '*';
+    if(f1.x < f2.x){
+        for (int i = (int)(f1.x); i <= (int)(f2.x); ++i){
+            int y = (int)((gradient * i) + remain);
+            m[i + XPLUS][y + YPLUS] = '*';
+        }
+    }
+    else{
+        for (int i = (int)(f2.x); i <= (int)(f1.x); ++i){
+            int y = (int)((gradient * i) + remain);
+            m[i + XPLUS][y + YPLUS] = '*';
+        }
     }
 }
--- 3D/main.cpp
+++ 3D/main.cpp
@@ -23,7 +23,25 @@ int main(int argc, char **argv){
     std::cout.tie(0), std::ios_base::sync_with_stdio(false);
     // int num = *argv[argc - 1] - '0';
     std::vector<std::vector<char> > m(100, std::vector<char>(60, ' '));
-    Point x1(0, 0, 0), x2(20, 0, 0), x3(0, 15, 0), x4(20, 15, 0), x5(0, 15, 10), x6(20, 15, 10), x7(0, 0, 10), x8(20, 0, 10);
+    // Point x[8] = {
+    //     {0, 0, 0},
+    //     {20, 0, 0},
+    //     {20, 15, 0},
+    //     {0, 15, 0},
+    //     {20, 15, 10},
+    //     {0, 0, 10},
+    //     {20, 0, 10},
+    // };
+    for (int i = 59; i >= 0; --i)
+    {
+        for (int j = 0; j < 100; ++j)
+        {
+            if(j == 0 || j == 99 || i == 0 || i ==59) m[j][i] = '.';
+        }
+    }
+    Point x1(10, 10, 0), x2(30, 10, 0), x3(10, 25, 0), x4(30, 25, 0), x5(10, 25, 10), x6(30, 25, 10), x7(10, 10, 10), x8(30, 10, 10);
+    // Point x1(0, 0, 0), x2(0, 20, 0), x3(-15, 0, 0), x4(-15, 20, 0);
+    // , x5(0, 15, 10), x6(20, 15, 10), x7(0, 0, 10), x8(20, 0, 10);
     // std::cout << x6.x << ' ' << x6.y << ' ' << x6.z << '\n';
     // x6.Y_Rotate(10);
     // std::cout << x6.x << ' ' << x6.y << ' '   << x6.z << '\n';
@@ -36,17 +54,42 @@ int main(int argc, char **argv){
     // Line l1(x1, x2);
 
 
-    std::cout << x1.x << ' ' << x1.y << ' ' << x1.z << '\n';
-    std::cout << x2.x << ' ' << x2.y << ' ' << x2.z << '\n';
-    std::cout << x3.x << ' ' << x3.y << ' ' << x3.z << '\n';
-    std::cout << x4.x << ' ' << x4.y << ' ' << x4.z << '\n';
-    std::cout << '\n'
-              << '\n';
-    x1.Z_Rotate(90);
-    x2.Z_Rotate(90);
-    x3.Z_Rotate(90);
-    x4.Z_Rotate(90);
+    // std::cout << x1.x << ' ' << x1.y << ' ' << x1.z << '\n';
+    // std::cout << x2.x << ' ' << x2.y << ' ' << x2.z << '\n';
+    // std::cout << x3.x << ' ' << x3.y << ' ' << x3.z << '\n';
+    // std::cout << x4.x << ' ' << x4.y << ' ' << x4.z << '\n';
+    // std::cout << '\n'
+    //           << '\n';
+    // for(Point &p:x){
+    //     p.Z_Rotate(110);
+    // }
+    x1.Z_Rotate(10);
+    x2.Z_Rotate(10);
+    x3.Z_Rotate(10);
+    x4.Z_Rotate(10);
+    x5.Z_Rotate(10);
+    x6.Z_Rotate(10);
+    x7.Z_Rotate(10);
+    x8.Z_Rotate(10);
+
     Line l1(x1, x2), l2(x1, x3), l3(x2, x4), l4(x3, x4), l5(x3, x5), l6(x4, x6), l7(x5, x6), l8(x7, x5), l9(x8, x6), l10(x1, x7), l11(x2, x8), l12(x7, x8);
+    // Line l[12] = {
+    //     {x[1], x[2]},
+    //     {x[1], x[3]},
+    //     {x[2], x[4]},
+    //     {x[3], x[4]},
+    //     {x[3], x[5]},
+    //     {x[4], x[6]},
+    //     {x[5], x[6]},
+    //     {x[7], x[5]},
+    //     {x[8], x[6]},
+    //     {x[1], x[7]},
+    //     {x[2], x[8]},
+    //     {x[7], x[8]},
+    // };
+    // for(Line &L:l){
+    //     L.Draw(m);
+    // }
     // l1.Draw(m);
 
     // l1.Draw(m);
@@ -54,10 +97,6 @@ int main(int argc, char **argv){
     // l3.Draw(m);
     // l4.Draw(m);
 
-    // x1.Z_Rotate(90);
-    // x2.Z_Rotate(90);
-    // x3.Z_Rotate(90);
-    // x4.Z_Rotate(90);
     // l1 = {x1, x2};
     // l2 = {x1, x3};
     // l3 = {x2, x4};
@@ -68,14 +107,14 @@ int main(int argc, char **argv){
     l2.Draw(m);
     l3.Draw(m);
     l4.Draw(m);
-    // l5.Draw(m);
-    // l6.Draw(m);
-    // l7.Draw(m);
-    // l8.Draw(m);
-    // l9.Draw(m);
-    // l10.Draw(m);
-    // l11.Draw(m);
-    // l12.Draw(m);
+    l5.Draw(m);
+    l6.Draw(m);
+    l7.Draw(m);
+    l8.Draw(m);
+    l9.Draw(m);
+    l10.Draw(m);
+    l11.Draw(m);
+    l12.Draw(m);
     // thread t1();
     // thread t2();
     // t1.join();
